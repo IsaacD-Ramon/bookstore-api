2d3cf8f69f28ce4ce4e4a9f26fbfb6e57ce5ca92
IsaacD-Ramon <isaacramondg@hotmail.com>
2024-02-06 13:24:06 -0300

    commite repositories

---
+++ src/main/java/com/isaac/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.isaac.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro,Integer>{
+
+}

27535e2c83d2d7f738aa7ba6e991e97bcbfb302c
IsaacD-Ramon <isaacramondg@hotmail.com>
2024-02-07 14:56:04 -0300

    perfil de teste implementado

--- src/main/java/com/isaac/bookstore/BookstoreApplication.java
+++ src/main/java/com/isaac/bookstore/BookstoreApplication.java
@@ -1,45 +1,13 @@
 package com.isaac.bookstore;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.isaac.bookstore.domain.Categoria;
-import com.isaac.bookstore.domain.Livro;
-import com.isaac.bookstore.repositories.CategoriaRepository;
-import com.isaac.bookstore.repositories.LivroRepository;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner {
-
-	
-	@Autowired
-	private LivroRepository livroRepository;
-
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-
+public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		
-		Categoria cat1 = new Categoria(0, "Amor", "o livor sobre seu amor isinhaaaaaaaa");
-
-		Livro l1 = new Livro(0, "meu grande amorrr", "sobre meu grande amor isinhaa",
-		 "o livro retrato sobre o amor da autora", cat1);
-
-		 cat1.getLivros().addAll(Arrays.asList(l1));
-
-		 this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		 this.livroRepository.saveAll(Arrays.asList(l1));
-
 	}
 
 }
---
+++ src/main/java/com/isaac/bookstore/service/DBService.java
@@ -0,0 +1,35 @@
+package com.isaac.bookstore.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.isaac.bookstore.domain.Categoria;
+import com.isaac.bookstore.domain.Livro;
+import com.isaac.bookstore.repositories.CategoriaRepository;
+import com.isaac.bookstore.repositories.LivroRepository;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public void instanciaBaseDados() {
+
+        Categoria cat1 = new Categoria(0, "Amor", "o livor sobre seu amor isinhaaaaaaaa");
+
+        Livro l1 = new Livro(0, "meu grande amorrr", "sobre meu grande amor isinhaa",
+                "o livro retrato sobre o amor da autora", cat1);
+
+        cat1.getLivros().addAll(Arrays.asList(l1));
+
+        this.categoriaRepository.saveAll(Arrays.asList(cat1));
+        this.livroRepository.saveAll(Arrays.asList(l1));
+
+    }
+}

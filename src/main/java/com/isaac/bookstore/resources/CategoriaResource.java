da1fcac0e47f05ff8e78bb675f96fdbf2e54a689
IsaacD-Ramon <isaacramondg@hotmail.com>
2024-02-19 20:50:30 -0300

    findAll para categoria

--- src/main/java/com/isaac/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/isaac/bookstore/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.isaac.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isaac.bookstore.domain.Categoria;
+import com.isaac.bookstore.dtos.CategoriaDTO;
 import com.isaac.bookstore.service.CategoriaService;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,5 +28,12 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> findById(@PathVariable Integer id){
         Categoria obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+    	List<Categoria> list = service.findAll();
+    	List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+    	return ResponseEntity.ok(listDTO);
     }
 }
